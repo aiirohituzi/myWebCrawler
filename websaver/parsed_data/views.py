fd86a85f5f70ff4f68a248e4c07fd6fef925a081
aiirohituzi <sinn5656@gmail.com>
2017-09-01 22:28:10 +0900

    Add view that receives rating data.

--- websaver/parsed_data/views.py
+++ websaver/parsed_data/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from parsed_data.models import RatingData
+import json
 
 # Create your views here.
+def getRating(request):
+    data = []
+    for r in RatingData.objects.all():
+        data.append({
+            'id': r.id,
+            'USER': r.userName,
+            'SOLO': r.solo,
+            'DUO': r.duo,
+            'SQUAD': r.squad,
+        })
+    data = json.dumps(data, indent=4)
+    print(data)
+    return HttpResponse(data, content_type = "application/json")
--- websaver/websaver/urls.py
+++ websaver/websaver/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from parsed_data.views import getRating
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^rating/$', getRating, name='getRating'),
 ]
