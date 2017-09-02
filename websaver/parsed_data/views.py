f503da84f2866689f9acd9365cce4ad14a8268d3
aiirohituzi <sinn5656@gmail.com>
2017-09-02 23:47:52 +0900

    Change display method to table. Add update date to column.

--- websaver/parsed_data/views.py
+++ websaver/parsed_data/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from parsed_data.models import RatingData
 import json
+import datetime
 
 # Create your views here.
 def getRating(request):
@@ -13,8 +14,24 @@ def getRating(request):
             'SOLO': r.solo,
             'DUO': r.duo,
             'SQUAD': r.squad,
+            'Update_time': datetime.datetime.strftime(r.created_at, "%Y-%m-%d %H:%M:%S"),
         })
     data = json.dumps(data, indent=4)
     print("Get - rating data")
     # print(data)
+    return HttpResponse(data, content_type = "application/json")
+
+def getRecentRating(request):
+    data = []
+    # for r in RatingData.objects.filter():
+    #     data.append({
+    #         'id': r.id,
+    #         'USER': r.userName,
+    #         'SOLO': r.solo,
+    #         'DUO': r.duo,
+    #         'SQUAD': r.squad,
+    #         'Update_time': datetime.datetime.strftime(r.created_at, "%Y-%m-%d %H:%M:%S"),
+    #     })
+    data = json.dumps(data, indent=4)
+    print("Get - recent rating data")
     return HttpResponse(data, content_type = "application/json")
