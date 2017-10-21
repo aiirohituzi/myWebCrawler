e09af91b45355294c16249bcd3c0bf07982cd39c
aiirohituzi <sinn5656@gmail.com>
2017-10-21 21:46:30 +0900

    Add fpp k/d data to the model.

--- websaver/parsed_data/models.py
+++ websaver/parsed_data/models.py
@@ -3,15 +3,23 @@ from django.db import models
 # Create your models here.
 class RatingData(models.Model):
     userName = models.CharField(max_length=30)
+    
     solofpp = models.CharField(max_length=5, null=True)
     duofpp = models.CharField(max_length=5, null=True)
     squadfpp = models.CharField(max_length=5, null=True)
+
     solo = models.CharField(max_length=5, null=True)
     duo = models.CharField(max_length=5, null=True)
     squad = models.CharField(max_length=5, null=True)
+
     solokd = models.CharField(max_length=5, null=True)
     duokd = models.CharField(max_length=5, null=True)
     squadkd = models.CharField(max_length=5, null=True)
+
+    solofppkd = models.CharField(max_length=5, null=True)
+    duofppkd = models.CharField(max_length=5, null=True)
+    squadfppkd = models.CharField(max_length=5, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
